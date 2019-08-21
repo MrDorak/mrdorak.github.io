48cbc48647b2851f3038a32db0021c6d10450a7b
Dorian <dorianturle@hotmail.fr>
2019-08-21 11:22:51 +0200

    console logged nuxtServerInit

--- store/index.js
+++ store/index.js
@@ -1,7 +1,8 @@
 const cookieparser = process.server ? require('cookieparser') : undefined;
 
 export const actions = {
-  nuxtServerInit({ commit }, { req }) { //not working idk why
+  async nuxtServerInit({ commit }, { req }) { //not working idk why
+    console.log('working!');
     let auth = null;
     if (req.headers.cookie) {
       const parsed = cookieparser.parse(req.headers.cookie);
