c31c94ce9c4f3b7ba64f5c822f95a00e14bd83b0
Dorian <dorianturle@hotmail.fr>
2019-08-21 12:23:23 +0200

    Updates

--- middleware/notAuthenticated.js
+++
@@ -1,6 +0,0 @@
-export default function ({ store, redirect }) {
-  // Si l'utilisateur est non authentifié, aller à la page d'accueil
-  if (!store.state.auth.auth) {
-    return redirect('/')
-  }
-}
